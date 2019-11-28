8a6403943e80ef8e08c12a0ff4622f1e22827d73
Karen Quiceno <ksquiceno3@misena.edu.co>
2019-11-28 12:37:08 -0500

    Esta guardado todo los archivos de Django

---
+++ platzigram/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\aprendiz\documents\proyecto1\platzigram\platzigram\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
